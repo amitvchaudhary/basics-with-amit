ee07a92658d28ae7af0cee43d8dbf809c7c62703
Amit Chaudhary <Amit.Chaudhary4@ibm.com>
2022-10-28 17:41:14 +0530

    update config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+export default {
+  target: "static",
+  router: {
+    base: "/basics-with-amit/",
+  },
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss"],
   content: {
-		markdown: {
-			toc: {
-				depth: 3,
-				searchDepth: 3,
-			},
-		},
-		highlight: {
-			theme: "dracula-soft",
-		},
-	},
-})
+    markdown: {
+      toc: {
+        depth: 3,
+        searchDepth: 3,
+      },
+    },
+    highlight: {
+      theme: "dracula-soft",
+    },
+  },
+};
