5b118ba7894768f4ca97b5214bf936d19c6ba04f
Amit Chaudhary <Amit.Chaudhary4@ibm.com>
2022-11-29 16:37:05 +0530

    feat: mysterious conn half

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -7,7 +7,7 @@
         <ContentDoc>
           <template v-slot="{ doc }">
             <ContentRenderer
-              class="prose prose-sm lg:prose-xl prose-slate max-w-none post-link post-subtitle"
+              class="prose prose-xl prose-slate max-w-none post-link post-subtitle"
               :value="doc"
             >
             </ContentRenderer>
