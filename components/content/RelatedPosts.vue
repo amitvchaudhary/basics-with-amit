2f757222de6b2b5f1474437f2476551bc88bf9ae
amitvchaudhary <amit.chaudhary140@gmail.com>
2023-01-03 18:16:26 +0530

    docs: update 1

---
+++ components/content/RelatedPosts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-6 border-2 border-pink-500 rounded-md bg-pink-50">
+    <div class="flex font-semibold text-pink-700">Related Posts</div>
+    <div class="post-link">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.post-link {
+  @apply prose-a:border-b-0 prose-a:text-red-900 prose-a:pb-1 prose-a:no-underline;
+}
+</style>
